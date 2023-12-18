5009a492de0e42644cf749f967e0fb74fc82fe47
BmoXD <bernhardsbergholds@gmail.com>
2023-12-18 18:25:27 +0200

    Comments are saved in tables that have the names of YT channel IDs

--- get_comments.py
+++ get_comments.py
@@ -42,6 +42,12 @@ def download_comments(video_url):
         'getcomments': True,
     }
 
+    video_data = {
+    'channel_id': '',
+    'video_id': '',
+    'comments': []
+    }
+
     # Check if cookies is not an empty string before adding it to ydl_opts
     if cookies:
         ydl_opts['cookiefile'] = cookies
@@ -49,15 +55,16 @@ def download_comments(video_url):
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(video_url, download=False)
-            comments = info_dict.get('comments', [])
-            video_id = info_dict.get('id')
-            log.debug(video_id)
-            #log.debug(info_dict)
+            video_data['comments'] = info_dict.get('comments', [])
+            video_data['video_id'] = info_dict.get('id')
+            video_data['channel_id'] = info_dict.get('channel_id')
+            #log.debug(video_id)
+            log.debug(info_dict)
     except:
         log.exception('')
 
-    log.debug("Downloaded %d comments for video: %s", len(comments), video_url)
-    return (comments, video_id)
+    log.debug("Downloaded %d comments for video: %s", len(video_data['comments']), video_url)
+    return video_data
 
 def save_to_json(comments, output_file='comments.json'):
     log.info("Saving comments to JSON file: %s", output_file)
@@ -80,39 +87,63 @@ def print_comments(comments):
         formatted_comment = f"[{comment_id}] : [{timestamp}] : [{username}] : {comment_text}"
         log.debug(formatted_comment)
 
-def save_to_db(video_id, comments):
+def save_to_db(video_data):
     global connection
+
+    channel_id = video_data['channel_id']
+    video_id = video_data['video_id']
+    comments = video_data['comments']
+
     try:
         cursor = connection.cursor()
 
-        # Create a new table for the specific video
-        create_table_query = f"""
-            CREATE TABLE IF NOT EXISTS {video_id} (
-                id INT AUTO_INCREMENT PRIMARY KEY,
-                comment_id VARCHAR(255),
-                timestamp DATETIME,
-                username VARCHAR(255),
-                comment_text TEXT
-            )
-        """
-        cursor.execute(create_table_query)
-        connection.commit()
+        # Checking if the table for the channel already exists
+        #cursor.execute(f"SHOW TABLES LIKE '`{channel_id.lower()}`'")
 
-        log.info("Table "+ str(video_id) +" created successfully.")
+        cursor.execute(f"SELECT COUNT(*) FROM information_schema.tables WHERE table_name = '{channel_id}'")
+        aaaa = cursor.fetchone()[0]
+        print("adsadssadsad: "+ str(aaaa))
+        if aaaa <= 0:
+            log.info("This video of id ["+video_id+"] is from a channel of id ["+channel_id+"] never seen before. Creating new table!")
+            # Create a new table for the specific video
+            create_table_query = f"""
+                CREATE TABLE IF NOT EXISTS `{channel_id}` (
+                    id INT AUTO_INCREMENT PRIMARY KEY,
+                    video_id VARCHAR(255),
+                    comment_id VARCHAR(255),
+                    timestamp DATETIME,
+                    username VARCHAR(255),
+                    comment_text TEXT
+                )
+            """
+            cursor.execute(create_table_query)
+            connection.commit()
+
+            log.info("Table "+ channel_id +" created successfully.")
 
         for comment in comments:
             comment_id = comment.get('id', '')
+
+            # Check if the comment_id already exists in the table
+            check_query = f"SELECT * FROM `{channel_id}` WHERE comment_id = %s"
+            cursor.execute(check_query, (comment_id,))
+            existing_comment = cursor.fetchone()
+
+            if existing_comment:
+                 log.info("Comment with comment_id ["+comment_id+"] already exists in the table. Skipping insertion.")
+                 continue
+
             timestamp = comment.get('timestamp', '')
-            formated_timestamp = datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+            formated_timestamp = datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S') # Unix time to human readable time
             username = comment.get('author', '')
             comment_text = comment.get('text', '')
 
             # Insert the comment into the MySQL database
             insert_query = f"""
-                INSERT INTO {video_id} (comment_id, timestamp, username, comment_text)
-                VALUES (%s, %s, %s, %s)
+                INSERT INTO `{channel_id}` (video_id, comment_id, timestamp, username, comment_text)
+                VALUES (%s, %s, %s, %s, %s)
             """
-            query_values = (comment_id, formated_timestamp, username, comment_text)
+            query_values = (video_id, comment_id, formated_timestamp, username, comment_text)
             cursor.execute(insert_query, query_values)
             connection.commit()
 
@@ -133,12 +164,12 @@ def main():
         video_url = input("Enter YouTube video URL or ID: ")
         log.debug("User input video URL or ID: %s", video_url)
     
-    comments = download_comments(video_url)
+    video_data = download_comments(video_url)
 
-    if comments:
-        print_comments(comments[0])
-        save_to_json(comments[0])
-        save_to_db(comments[1], comments[0])
+    if video_data:
+        print_comments(video_data['comments'])
+        save_to_json(video_data['comments'])
+        save_to_db(video_data)
     else:
         log.warning("No comments found for the given video.")
 
